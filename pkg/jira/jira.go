000f8c9c49e01613d680d12ead5b086057e6cfdd
Killean Johnson <killeanjohnson06@gmail.com>
2022-09-28 19:48:52 +0000

    Adding jira api struct

--- pkg/jira/jira.go
+++ pkg/jira/jira.go
@@ -1,1 +1,28 @@
 package jira
+
+import (
+	"log"
+
+	jiraAPI "github.com/andygrunwald/go-jira"
+)
+
+type Client struct {
+	client *jiraAPI.Client
+}
+
+func (c *Client) Connect(apiToken string, username string, url string) {
+	// Set up client
+	authTransport := jiraAPI.BasicAuthTransport{
+		Username: username,
+		Password: apiToken,
+	}
+
+	client, err := jiraAPI.NewClient(authTransport.Client(), url)
+
+	if err != nil {
+		log.Print("Error connecting to jira!")
+		log.Fatal(err)
+	} else {
+		c.client = client
+	}
+}
